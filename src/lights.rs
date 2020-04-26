4073780741c4f9994fab3ea1118b963356e55926
Jacek Bilski <jacek@bilski.tech>
2020-04-26 20:48:33 +0200

    Preparations for multiple lights.

--- src/lights.rs
+++ src/lights.rs
@@ -8,6 +8,10 @@ use cgmath::prelude::*;
 
 use crate::shader::LIGHTS_UBO_BINDING_POINT;
 
+use self::gl::types::*;
+
+const MAX_LIGHTS: u8 = 4;
+
 struct Light {
     position: Point3<f32>,
     ambient: Vector3<f32>,
@@ -31,7 +35,8 @@ impl Lights {
             gl::GenBuffers(1, &mut light_ubo);
             gl::BindBuffer(gl::UNIFORM_BUFFER, light_ubo);
             let vector3_size = mem::size_of::<Vector4<f32>>() as isize; // there's no mistake, Vector3 takes the same amount of memory as Vector4
-            gl::BufferData(gl::UNIFORM_BUFFER, 4 * vector3_size, ptr::null(), gl::STATIC_DRAW);
+            let int_size = mem::size_of::<GLint>() as isize;
+            gl::BufferData(gl::UNIFORM_BUFFER, int_size + MAX_LIGHTS as isize * 4 * vector3_size, ptr::null(), gl::STATIC_DRAW);
             gl::BindBufferBase(gl::UNIFORM_BUFFER, LIGHTS_UBO_BINDING_POINT, light_ubo);
             gl::BindBuffer(gl::UNIFORM_BUFFER, 0);
             light_ubo
@@ -39,15 +44,20 @@ impl Lights {
     }
 
     pub fn add(&mut self, position: Point3<f32>, ambient: Vector3<f32>, diffuse: Vector3<f32>, specular: Vector3<f32>) {
-        let vector3_size = mem::size_of::<Vector4<f32>>() as isize;
+        let lights_no= self.lights.len() as isize;
         let light = Light {position, ambient, diffuse, specular};
-        self.lights  = vec![light]; // so far only one is supported
+        self.lights.push(light);
         unsafe {
+            let int_size = mem::size_of::<GLint>() as isize;
+            let vector3_size = mem::size_of::<Vector4<f32>>() as isize;
+            let light_size = 4 * vector3_size;
+            println!("lights_no: {}, int_size: {}, vector3_size: {}, light_size: {}", lights_no, int_size, vector3_size, light_size);
             gl::BindBuffer(gl::UNIFORM_BUFFER, self.ubo);
-            gl::BufferSubData(gl::UNIFORM_BUFFER, 0, vector3_size, position.as_ptr() as *const c_void);
-            gl::BufferSubData(gl::UNIFORM_BUFFER, 1 * vector3_size, vector3_size, ambient.as_ptr() as *const c_void);
-            gl::BufferSubData(gl::UNIFORM_BUFFER, 2 * vector3_size, vector3_size, diffuse.as_ptr() as *const c_void);
-            gl::BufferSubData(gl::UNIFORM_BUFFER, 3 * vector3_size, vector3_size, specular.as_ptr() as *const c_void);
+            gl::BufferSubData(gl::UNIFORM_BUFFER, 0, int_size, &lights_no as *const isize as *const c_void);
+            gl::BufferSubData(gl::UNIFORM_BUFFER, 16 + lights_no * light_size + 0 * vector3_size, vector3_size, position.as_ptr() as *const c_void);
+            gl::BufferSubData(gl::UNIFORM_BUFFER, 16 + lights_no * light_size + 1 * vector3_size, vector3_size, ambient.as_ptr() as *const c_void);
+            gl::BufferSubData(gl::UNIFORM_BUFFER, 16 + lights_no * light_size + 2 * vector3_size, vector3_size, diffuse.as_ptr() as *const c_void);
+            gl::BufferSubData(gl::UNIFORM_BUFFER, 16 + lights_no * light_size + 3 * vector3_size, vector3_size, specular.as_ptr() as *const c_void);
             gl::BindBuffer(gl::UNIFORM_BUFFER, 0);
         }
     }
--- src/xmas_tree/shaders/static.frag
+++ src/xmas_tree/shaders/static.frag
@@ -26,23 +26,24 @@ layout (std140) uniform Camera {
 };
 
 layout (std140) uniform Lights {
-    Light light;
+    int lightsNo;
+    Light light[4];
 };
 
 out vec4 FragColor;
 
 void main() {
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light[0].ambient * material.ambient;
 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPosition);
+    vec3 lightDir = normalize(light[0].position - FragPosition);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * light.diffuse * material.diffuse;
+    vec3 diffuse = diff * light[0].diffuse * material.diffuse;
 
     vec3 viewDir = normalize(cameraPosition - FragPosition);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = spec * light.specular * material.specular;
+    vec3 specular = spec * light[0].specular * material.specular;
 
     FragColor = vec4((ambient + diffuse + specular), 1.0);
 }
